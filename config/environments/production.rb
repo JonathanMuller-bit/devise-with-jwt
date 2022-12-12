fc72d87119c843e7339df29de69a3d97bff7d102
Jonathan <jonathanfelipe.muller@gmail.com>
2022-12-12 10:27:10 -0300

    set up devise and migrations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Devise Stuff - When go on production, replace URL with a valid host
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
